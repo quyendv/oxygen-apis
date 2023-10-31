4ed985a0be7e5184c549dce9b00a9b6000c4617b
quyendv <daovanquyen45@gmail.com>
2023-11-01 00:47:44 +0700

    feat: setup linter, formater, refactor convention

--- routes/weatherRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const weatherController = require("../controllers/weatherController");
-router.get('/current',weatherController.current);
-
-
-
-module.exports = router;
