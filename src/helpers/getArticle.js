4008e3cb9615e33a74689f1b08e7c712bbb4efe7
Đỗ Trọng Thư <20020157@vnu.edu.vn>
2023-11-14 22:36:39 +0700

    feat: Fix path on linux (#8)
    Co-authored-by: trongthu3212 <dtt3212@gmail.com>

--- src/helpers/getArticle.js
+++ src/helpers/getArticle.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 let article = [];
 function getArticleFromSrc() {
-  article = JSON.parse(fs.readFileSync(`${__dirname}\\data.txt`, 'utf8'));
+  article = JSON.parse(fs.readFileSync(`${__dirname}/data.txt`, 'utf8'));
 }
 function getArticleList() {
   return article;
