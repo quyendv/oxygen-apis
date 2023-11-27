e0c23b75985d3ec749ae8bbe595f1efb92b2a47c
ADMIN <gsu0987q@gmail.com>
2023-11-27 20:24:29 +0700

    fix: locationlist

--- src/controllers/locationlist.controller.js
+++ src/controllers/locationlist.controller.js
@@ -7,26 +7,38 @@ async function getCities(req, res, next) {
   return res.status(200).json(cities);
 }
 async function getDistricts(req, res, next) {
-  const codename = req.query.codename;
-  const city = await db.City.findOne({ where: { codename } });
-  const districts = await db.District.findAll({
-    where: {
-      city_code: city.code,
-    },
-  });
+  try {
+    const codename = req.query.codename;
+    const city = await db.City.findOne({ where: { codename } });
+    const districts = await db.District.findAll({
+      where: {
+        city_code: city.code,
+      },
+    });
 
-  return res.status(200).json(districts);
+    return res.status(200).json(districts);
+  } catch (error) {
+    return res.status(400).json({
+      message: 'something went wrong',
+    });
+  }
 }
 
 async function getWards(req, res, next) {
-  const codename = req.query.codename;
-  const district = await db.District.findOne({ where: { codename } });
-  const wards = await db.Ward.findAll({
-    where: {
-      district_code: district.code,
-    },
-  });
+  try {
+    const codename = req.query.codename;
+    const district = await db.District.findOne({ where: { codename } });
+    const wards = await db.Ward.findAll({
+      where: {
+        district_code: district.code,
+      },
+    });
 
-  return res.status(200).json(wards);
+    return res.status(200).json(wards);
+  } catch (error) {
+    return res.status(400).json({
+      message: 'something went wrong',
+    });
+  }
 }
 export default { getCities, getDistricts, getWards };
