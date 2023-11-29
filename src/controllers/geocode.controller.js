ec63e51833468cec0e2854a06d5caafb6bd8f8b7
ADMIN <gsu0987q@gmail.com>
2023-11-29 20:31:23 +0700

    feat: geocode

--- src/controllers/geocode.controller.js
+++ src/controllers/geocode.controller.js
@@ -15,8 +15,31 @@ async function reverseGeocode(req, res, next) {
     'GET',
     `${BASE_URL}/reverse?lat=${lat}&lon=${lon}&apiKey=${API_KEY}`,
   );
+  const finalResult = {
+    name: result.features[0].properties.name ?? '',
+    country: result.features[0].properties.country ?? '',
+    country_code: result.features[0].properties.country_code ?? '',
+    province: result.features[0].properties.state
+      ? result.features[0].properties.state
+      : result.features[0].properties.city
+      ? result.features[0].properties.city
+      : '',
+    district: result.features[0].properties.district
+      ? result.features[0].properties.district
+      : result.features[0].properties.county
+      ? result.features[0].properties.county
+      : '',
+    ward: result.features[0].properties.quarter
+      ? result.features[0].properties.quarter
+      : result.features[0].properties.suburb
+      ? result.features[0].properties.suburb
+      : '',
 
-  return res.status(200).json(result.features[0].properties);
+    lat: result.features[0].properties.lat,
+    lon: result.features[0].properties.lon,
+  };
+  finalResult[finalResult.result_type] = finalResult['result_type'];
+  return res.status(200).json(result);
 }
 async function getRelatedLocation(req, res, next) {
   const text = req.query.text.trim();
@@ -33,8 +56,30 @@ async function getRelatedLocation(req, res, next) {
     encodeURI(`${BASE_URL}/autocomplete?text=${text}&apiKey=${API_KEY}`),
   );
   const finalResult = result.features.map((el, i) => {
-    return el.properties;
+    return {
+      name: el.properties.name ?? '',
+      country: el.properties.country ?? '',
+      country_code: el.properties.country_code ?? '',
+      province: el.properties.state
+        ? el.properties.state
+        : el.properties.city
+        ? el.properties.city
+        : '',
+      district: el.properties.district
+        ? el.properties.district
+        : el.properties.county
+        ? el.properties.county
+        : '',
+      ward: result.features[0].properties.quarter
+        ? result.features[0].properties.quarter
+        : result.features[0].properties.suburb
+        ? result.features[0].properties.suburb
+        : '',
+      lat: el.properties.lat,
+      lon: el.properties.lon,
+    };
   });
+  finalResult[finalResult.result_type] = finalResult['result_type'];
   return res.status(200).json(finalResult);
 }
 export default { reverseGeocode, getRelatedLocation };
